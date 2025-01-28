4b7f6b38252028fa6b84062fcde3c8e2550fe282
BugHunter-X <askzenai@gmail.com>
2025-01-28 18:40:05 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,21 @@
+function foo(a, b) {
+  return a + b;
+}
+
+function bar(a, b) {
+  return foo(a, b) * 2;
+}
+
+console.log(bar(2, 3)); // Output: 10
+
+// The bug is that the bar function does not handle non-numeric input.
+
+function bar(a, b) {
+  if (typeof a !== 'number' || typeof b !== 'number') {
+    throw new Error('Inputs must be numbers');
+  }
+  return foo(a, b) * 2;
+}
+
+console.log(bar(2, 3)); // Output: 10
+console.log(bar('2', 3)); // throws error
